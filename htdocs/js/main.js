a68267652b0f3665ae90575997c50d9acf96f87d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-21 22:22:33 +0900

    データを追加していくテーブル

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -7,6 +7,16 @@ new Vue({
     data: function () {
         return {
             title: 'Vue App',
+            datas: [],
         }
-    }
+    },
+    methods: {
+        addData: function () {
+            const id = this.datas.length + 1
+            this.datas.push({
+                id: id,
+                msg: ['foo', 'bar', 'baz'].find((_, index) => index === this.datas.length % 3)
+            })
+        }
+    },
 })
