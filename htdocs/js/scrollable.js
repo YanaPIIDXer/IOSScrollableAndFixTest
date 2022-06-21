28f56346792fc621211c05a83efd7f4f2ca6cb09
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-22 03:05:44 +0900

    バウンススクロール抑制

--- htdocs/js/scrollable.js
+++ htdocs/js/scrollable.js
@@ -4,10 +4,31 @@ $(function () {
     $scrollable.each(function () {
         this.scrollTop = 1
     })
+
+    let prevY = 0
+    $scrollable.on('touchstart', function (e) {
+        prevY = e.pageY
+    })
+
+    $scrollable.on('touchmove', function (e) {
+        const y = e.pageY
+        const move = prevY - y      // スワイプした方向とは逆向きに動く
+        prevY = y
+
+        if (this.scrollTop + move <= 0) {
+            this.scrollTop = 1
+            e.preventDefault()
+        } else if (this.scrollTop + this.clientHeight + move >= this.scrollHeight) {
+            this.scrollTop = this.scrollHeight - 1
+            e.preventDefault()
+        }
+    })
     
     $scrollable.on('scroll', function () {
-        if (this.scrollTop === 0) {
+        if (this.scrollTop <= 0) {
             this.scrollTop = 1
+        } else if (this.scrollTop + this.clientHeight >= this.scrollHeight) {
+            this.scrollTop = this.scrollHeight - 1
         }
     })
 })
